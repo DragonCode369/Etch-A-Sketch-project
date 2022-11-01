ac6c6f80ec2c43280882de7294666f4850ba49b4
DragonCode369 <dragoncode369@gmail.com>
2022-11-01 22:50:48 +0100

    User input feature

--- jsfajl.js
+++ jsfajl.js
@@ -1,21 +1,49 @@
-
 const container = document.querySelector("#container");
+const button = document.getElementById("btn");
+
+// Starting values
+let grid = 16;
 let divs;
 let arrayDivs = [];
 
-for(let i=0; i<256; i++) {
-
+// Creating divs for grid-view
+for	(let i = 0; i < grid*grid; i++) {
 	divs = document.createElement("div");
 	divs.classList.add("divs");
 	container.appendChild(divs);
 
 	arrayDivs.push(divs);
 };
+	arrayDivs = Array.from(arrayDivs);
 
-arrayDivs = Array.from(arrayDivs);
+	arrayDivs.forEach((div) => {
+		
+		div.addEventListener("mouseenter", () => {
+			div.style.backgroundColor = "#000";
+		});
+	});
 
-arrayDivs.forEach((div) => {
-	div.addEventListener('mouseenter', () => {
-		div.style.backgroundColor = "#000";
+button.addEventListener("click", () => {
+
+	grid = prompt("Enter the number of Grid squares per side:", "16"); 
+	let divSize = (600/grid-2);
+	container.style = `grid-template-columns: repeat(${grid}, ${divSize}px); grid-template-rows: repeat(${grid}, ${divSize}px);`
+
+	for	(let i = 0; i < grid*grid; i++) {
+		divs = document.createElement("div");
+		divs.classList.add("divs");
+		container.appendChild(divs);
+
+		arrayDivs.push(divs);
+	};
+	
+	console.log(grid);
+	arrayDivs = Array.from(arrayDivs);
+
+	arrayDivs.forEach((div) => {
+		
+		div.addEventListener("mouseenter", () => {
+			div.style.backgroundColor = "#000";
+		});
 	});
 });
